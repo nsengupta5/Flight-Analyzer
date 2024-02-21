7e3b6e7e69c3a0deeebea76714a4b9f532c45ce6
Nikhil Sengupta <nikhil.sengupta10@protonmail.com>
2024-02-21 11:46:25 +0000

    Converted files

--- convert.py
+++ convert.py
@@ -1,8 +1,18 @@
 from pyspark.sql import SparkSession
+from os import listdir
+from os.path import isfile, join
 
 data_file = "airline.csv"
 parquet_file = "airline.parquet"
+supplementary_dir = "SupplementaryCSVs"
 
+# Main airline data
 spark = SparkSession.builder.appName("convert").getOrCreate()
-df = spark.read.format("csv").option("header", "true").load("airline.csv")
-df.write.format("parquet").save("airline.parquet")
+# airline_df = spark.read.format("csv").option("header", "true").load("airline.csv")
+# airline_df.write.format("parquet").save("./data/airline.parquet")
+
+# Supplementary data
+supplementary_files = [f for f in listdir(supplementary_dir) if isfile(join(supplementary_dir, f))]
+for file in supplementary_files:
+    df = spark.read.format("csv").option("header", "true").load(f"{supplementary_dir}/{file}")
+    df.write.format("parquet").save(f"./data/{file}.parquet")
